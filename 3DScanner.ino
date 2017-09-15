441a247b27676626118cc14d0391c8a127075ab7
prava-d <prava@students.olin.edu>
2017-09-15 10:22:54 -0400

    Add files via upload
    Main Arduino code for the 3D Scanner lab

---
+++ 3DScanner.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+// creates servo objects to control a servo
+Servo myservoPan; // turntable aspect
+Servo myservoTilt; // turns sensor 'up and down'
+
+// defines pins
+const int sensorPin = A0;
+int servoPanPin = 9;
+int servoTiltPin = 10;
+
+// defines values
+int sensorValue = 0; // stores sensor values
+// stores servo positions
+int servoPanPos = 0;
+int servoTiltPos = 0;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  myservoPan.attach(servoPanPin);
+  myservoTilt.attach(servoTiltPin);
+}
+
+void loop() {
+  // read value from sensor
+  sensorValue = analogRead(sensorPin);
+}
